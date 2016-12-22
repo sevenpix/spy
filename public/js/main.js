8353497597c9303271de7d16c9ecb6677abe2602
Mario Taferner <mario.taferner@gmail.com>
2016-12-22 19:58:32 +0100

    add app icon and adjust map  zoom

--- public/js/main.js
+++ public/js/main.js
@@ -90,7 +90,7 @@ socket.on('location', function(client){
     var name = client.name;
 
     if (!mymap) {
-        mymap = L.map('mapid').setView([lat, lon], 13);
+        mymap = L.map('mapid').setView([lat, lon], 16);
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
             maxZoom: 18,
